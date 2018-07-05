704bd2b060d8259fe9fec10aad8d20cf531254a7
vmetric <armada2526pbem@gmail.com>
2018-07-04 20:10:16 -0600

    Adds commentary/documentation
    Added commentary/documentation of the script, as well as a todo list.

--- dummyfiles.bat
+++ dummyfiles.bat
@@ -1,8 +1,27 @@
+REM Created by Matthew Putnam.
+
+REM ---- TODO ----
+REM Make it more... user-friendly?
+REM Add progress indicator
+REM Confirm information before execution
+
+
+
+REM Prompt for number of files to create.
 set /p numOfFiles="Amount of dummy files to create: "
+
+REM Prompt for the size to make the files, in MB.
 set /p sizeOfFiles="Size to make dummy files (in MB): "
+
+REM Prompt for the directory to create the files.
 set /p createLocation="Dir to create dummy files: "
+
+REM Converts size entered by user to bytes.
 set /a sizeOfFilesBytes=%sizeOfFiles% * 1000000
 
+REM For loop that uses fsutil to create the files, naming each file with 
+REM incrementing integer at the end.
 for /L %%I in (1,1,%numOfFiles%) do fsutil file createnew "%createLocation%\dummy%%I.txt" %sizeOfFilesBytes%
 
+REM Pauses script so it isn't just an abrupt closing of the cmd window.
 pause
